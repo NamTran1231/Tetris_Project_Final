6ab6a5e21429f81d1f12db0e494390bd0889ce59
Nam Tran <123750100+NamTran1231@users.noreply.github.com>
2026-04-30 15:30:36 +0700

    Update Piece.java

--- src/logic/Piece.java
+++ src/logic/Piece.java
@@ -1,5 +1,35 @@
 package logic;
 
 public class Piece {
+
+    // 7 loại hình Tetris
+    public static final int[][][] SHAPES = {
+        {{1,1,1,1}},           // I
+        {{1,1},{1,1}},         // O
+        {{0,1,0},{1,1,1}},     // T
+        {{0,1,1},{1,1,0}},     // S
+        {{1,1,0},{0,1,1}},     // Z
+        {{1,0,0},{1,1,1}},     // J
+        {{0,0,1},{1,1,1}}      // L
+    };
+
+    private int[][] shape;
+    private int x, y;
+    private int type;
+
+
+    public Piece(int type){
+        this.type = type;
+
+        int rows = SHAPES[type].length;
+        int cols = SHAPES[type][0].length;
+        this.shape = new int[rows][cols];
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+        this.shape[i][j] = SHAPES[type][i][j];
+        }
+    }
+}
     
 }
