c561982402a7c9921b199682b7e351568c83a6af
phonghoasem <phonghoasem@gmail.com>
2026-05-08 21:55:52 +0700

    save score to text and fix the score calculation

--- src/util/ScoreManager.java
+++ src/util/ScoreManager.java
@@ -1,5 +1,8 @@
 package util;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class ScoreManager {
     private static ScoreManager instance;
     private int highscore;
@@ -17,7 +20,42 @@ public class ScoreManager {
     }
 
     public int calculate(int lines, int level) {
-        return lines*100*level;
+        int baseScore = 0;
+        switch (lines) {
+            case 1:
+                baseScore = 100;
+                break;
+            case 2:
+                baseScore = 300;
+                break;
+            case 3:
+                baseScore = 500;
+                break;
+            case 4:
+                baseScore = 800;
+                break;
+            default:
+                baseScore = 0;
+        }
+        return baseScore * level;
     }
 
+        private void saveHighscore() {
+        try (FileWriter writer = new FileWriter(SAVE_FILE)) {
+            writer.write(Integer.toString(highscore));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void updatehighscore (int score) {
+        if (score > highscore) {
+            highscore = score;
+            saveHighscore();
+        }
+    }
+
+    public int getHighscore() {
+        return highscore;
+    }
 }
