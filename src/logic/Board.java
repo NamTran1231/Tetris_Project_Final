e9beb04e93b80000b839add54e1b0a2b2ffb292c
Nam Tran <123750100+NamTran1231@users.noreply.github.com>
2026-05-08 23:12:42 +0700

    Update Board.java

--- src/logic/Board.java
+++ src/logic/Board.java
@@ -1,5 +1,30 @@
 package logic;
 
 public class Board {
-    
+
+    public static final int COLS = 10;
+    public static final int ROWS = 20;
+
+    private int[][] grid;
+
+    public Board() {
+        grid = new int[ROWS][COLS];
+    }
+
+    public int[][] getGrid() { return grid; }
+
+    public void reset() {
+        grid = new int[ROWS][COLS];
+    }
+
+    public void placePiece(Piece piece) {
+    int[][] shape = piece.getShape();
+    int px = piece.getX();
+    int py = piece.getY();
+
+    for (int i = 0; i < shape.length; i++)
+        for (int j = 0; j < shape[0].length; j++)
+            if (shape[i][j] != 0)
+                grid[py + i][px + j] = piece.getType() + 1;
+}
 }
