5b76735ce306fbfa7b52133cb27b9197372e62e2
phonghoasem <phonghoasem@gmail.com>
2026-05-08 21:19:11 +0700

    Calculate score

--- src/util/ScoreManager.java
+++ src/util/ScoreManager.java
@@ -1,5 +1,23 @@
 package util;
 
 public class ScoreManager {
+    private static ScoreManager instance;
+    private int highscore;
+    private static final String SAVE_FILE = "highscore.txt";
+
+    private ScoreManager() {
+        this.highscore = 0;
+    }
+
+    public static ScoreManager getInstance() {
+        if (instance == null) {
+            instance = new ScoreManager();
+        }
+        return instance;
+    }
+
+    public int calculate(int lines, int level) {
+        return lines*100*level;
+    }
 
 }
