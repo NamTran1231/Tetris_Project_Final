8452624c208bcd5e3ea6501d1742c891d83e70c5
phonghoasem <phonghoasem@gmail.com>
2026-05-09 22:39:19 +0700

    add generate random piece and reset board

--- src/logic/GameEngine.java
+++ src/logic/GameEngine.java
@@ -10,4 +10,56 @@ public class GameEngine {
     private int score;
     private boolean isGameOver;
     private int level;
+
+        public GameEngine() {
+        this.board = new Board();
+        this.score = 0;
+        this.level = 1;
+        this.isGameOver = false;
+        this.currentPiece = generateRandomPiece();
+        this.nextPiece = generateRandomPiece();
+    }
+
+    private Piece generateRandomPiece() {
+        int type = (int) (Math.random() * Piece.SHAPES.length);
+        return new Piece(type);
+    }
+
+    public void reset() {
+        board.reset();
+        score = 0;
+        level = 1;
+        isGameOver = false;
+        currentPiece = generateRandomPiece();
+        nextPiece = generateRandomPiece();
+
+        if (observer != null) {
+            observer.onBoardChanged(board.getGrid());
+        }
+    }
+
+    public Board getBoard() {
+        return board;
+    }
+
+    public Piece getCurrentPiece() {
+        return currentPiece;
+    }
+
+    public Piece getNextPiece() {
+        return nextPiece;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public boolean isGameOver() {
+        return isGameOver;
+    }    
 }
+
